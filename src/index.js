ac18af767a6240424570095129c0e1a66e866e33
G.Volkov <nwpgrifon@gmail.com>
2020-03-05 22:51:16 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(!array || !array.length) return 0;
+    return array.sort(function (a, b) {
+        return a - b;
+    })[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(!array || !array.length) return 0;
+    return array.sort(function (a, b) {
+        return b - a;
+    })[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(!array || !array.length) return 0;
+  return (array.reduce(function (a, b) {
+        return (a + b)
+  })) / array.length;
 }
